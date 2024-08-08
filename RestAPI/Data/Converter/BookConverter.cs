e06acf54dc075e822e60b264de3c3185842cdfee
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-07 22:54:54 -0300

    Implementing VO for book entity (for the VO we could use the Imapper lib)

---
+++ RestAPI/Data/Converter/BookConverter.cs
@@ -0,0 +1,51 @@
+﻿using RestAPI.Data.Converter.Interfaces;
+using RestAPI.Data.VO;
+using RestAPI.Model;
+
+namespace RestAPI.Data.Converter
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+
+            return new Book
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Launch_date = origin.Launch_date,
+                Price = origin.Price
+            };
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+
+            return new BookVO
+            {
+                Id = origin.Id,
+                Title = origin.Title,
+                Author = origin.Author,
+                Launch_date = origin.Launch_date,
+                Price = origin.Price
+            };
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ RestAPI/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestAPI.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string Author { get; set; }
+        public DateTime Launch_date { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+    }
+}
