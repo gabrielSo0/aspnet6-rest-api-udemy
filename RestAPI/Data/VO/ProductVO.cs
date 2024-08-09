4d49a71fdfa424658c11ccaa44ef00a469dc5e77
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-08 21:31:36 -0300

    Adding Custom Serialization, just in case you need to use it for future reference

---
+++ RestAPI/Data/VO/ProductVO.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestAPI.Data.VO
+{
+    public class ProductVO
+    {
+        [JsonPropertyName("code")]
+        public int Id { get; set; }
+        [JsonPropertyName("product_name")]
+        public string ProductName { get; set; }
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+    }
+}
