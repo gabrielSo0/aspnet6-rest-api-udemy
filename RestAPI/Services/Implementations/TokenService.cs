20a881047e3761547e04e076d762d99ad95f3f7f
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-13 22:16:19 -0300

    Creating some token settings to allow more flexibility when setting some values like issuer, secret key. They will be used on our AppSettings

---
+++ RestAPI/Services/Implementations/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using RestAPI.Configurations;
+using System.Security.Claims;
+
+namespace RestAPI.Services.Implementations
+{
+    public class TokenService : ITokenService
+    {
+        private TokenConfiguration _configuration;
+        public TokenService(TokenConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
