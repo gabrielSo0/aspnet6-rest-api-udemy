3430b3a70785ed4eb4e6e65b9f1477d052a39603
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-14 08:33:34 -0300

    Implementing the RefreshToken generation, and the GetPrincipalFromExpiredTokens inside TokenService

--- RestAPI/Services/Implementations/TokenService.cs
+++ RestAPI/Services/Implementations/TokenService.cs
@@ -2,6 +2,7 @@
 using RestAPI.Configurations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RestAPI.Services.Implementations
@@ -16,14 +17,14 @@ namespace RestAPI.Services.Implementations
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
             var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
-            var signinCredentias = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
             var options = new JwtSecurityToken(
                 issuer: _configuration.Issuer,
                 audience: _configuration.Audience,
                 claims: claims,
                 expires: DateTime.Now.AddMinutes(_configuration.Minutes),
-                signingCredentials: signinCredentias);
+                signingCredentials: signinCredentials);
 
             var stringToken = new JwtSecurityTokenHandler().WriteToken(options);
 
@@ -32,12 +33,39 @@ namespace RestAPI.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            };
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+            if (jwtSecurityToken == null ||
+                !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCulture)) 
+                throw new SecurityTokenException("Invalid Token");
+
+            return principal;
         }
     }
 }
