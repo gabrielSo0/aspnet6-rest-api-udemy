50d03bcb9e6cea634b842b0282d9742c24fb9805
Gabriel <gabrielsoliveira043@gmail.com>
2024-08-14 21:46:40 -0300

    Creating the AuthController to map the signin endpoint

---
+++ RestAPI/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Data.VO;
+using RestAPI.Services;
+using System;
+
+namespace RestAPI.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public AuthController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost()]
+        [Route("singin")]
+        public IActionResult Signin(UserVO user)
+        {
+            if(user == null) return BadRequest("Invalid Cliente Request");
+
+            var token = _loginService.ReturnUserToken(user);
+
+            if(token == null) return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
